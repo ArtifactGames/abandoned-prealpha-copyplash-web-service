d44ef4c737978618c1301e6a5fc70f5075df263b
Adoankim <adoankim@gmail.com>
2018-03-19 19:45:58 +0100

    Add serialize name to game modes and states enums

--- src/main/kotlin/com/artifactgames/copyplash/type/GameModes.kt
+++ src/main/kotlin/com/artifactgames/copyplash/type/GameModes.kt
@@ -1,5 +1,8 @@
 package com.artifactgames.copyplash.type
 
+import com.google.gson.annotations.SerializedName
+
 enum class GameModes {
+    @SerializedName("0")
     INSPIRATION,
 }
--- src/main/kotlin/com/artifactgames/copyplash/type/States.kt
+++ src/main/kotlin/com/artifactgames/copyplash/type/States.kt
@@ -1,6 +1,9 @@
 package com.artifactgames.copyplash.type
 
+import com.google.gson.annotations.SerializedName
+
 enum class States(state: Int) {
+    @SerializedName("0")
     START(0),
 
 }
