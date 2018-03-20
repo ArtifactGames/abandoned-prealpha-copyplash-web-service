08bc9795188712be4a06865e9c930a9fd2556d0a
Adoankim <adoankim@gmail.com>
2018-03-21 00:01:39 +0100

    Adds host and player connection/disconnection handler events

--- src/main/kotlin/com/artifactgames/copyplash/controller/ChannelController.kt
+++ src/main/kotlin/com/artifactgames/copyplash/controller/ChannelController.kt
@@ -5,9 +5,13 @@ import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.WebSocketMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.util.*
 
 @Component
 class ChannelController: TextWebSocketHandler() {
+
+    var host: WebSocketSession? = null
+    val playerList: LinkedList<WebSocketSession?> = LinkedList()
 
     override fun handleTransportError(session: WebSocketSession?, exception: Throwable?) {
         println("Error: ${exception.toString()}")
@@ -15,10 +19,16 @@ class ChannelController: TextWebSocketHandler() {
 
     override fun afterConnectionClosed(session: WebSocketSession?, status: CloseStatus?) {
         println("ConnectionClosed: ${session.toString()}")
+        playerList.remove(session)
     }
 
     override fun afterConnectionEstablished(session: WebSocketSession?) {
         println("ConnectionStablished: ${session.toString()}")
+        if (host == null) {
+            host = session
+        } else {
+            playerList.add(session)
+        }
     }
 
     override fun handleMessage(session: WebSocketSession?, message: WebSocketMessage<*>?) {
