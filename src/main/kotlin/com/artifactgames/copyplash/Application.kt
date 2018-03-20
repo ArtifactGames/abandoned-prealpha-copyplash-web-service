c4b757f7383f92093e0ce34935b5d8d1b8c2107c
Adoankim <adoankim@gmail.com>
2018-03-20 23:56:04 +0100

    Add support for Kotlin data classes serialization with Jackson

--- src/main/kotlin/com/artifactgames/copyplash/Application.kt
+++ src/main/kotlin/com/artifactgames/copyplash/Application.kt
@@ -1,11 +1,21 @@
 package com.artifactgames.copyplash
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import com.fasterxml.jackson.module.kotlin.*
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
+}
+
+@Bean
+@Primary
+fun objectMapper() = ObjectMapper().apply {
+    registerModule(KotlinModule())
 }
