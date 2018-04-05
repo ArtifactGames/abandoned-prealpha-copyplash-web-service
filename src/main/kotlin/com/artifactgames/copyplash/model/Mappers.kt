8df0cdca95c5a6b64f6497d177fe62dd9d08c90d
Adoankim <adoankim@gmail.com>
2018-04-06 00:31:51 +0200

    Refactored game mode and game settings models and auto load config at startup

--- src/main/kotlin/com/artifactgames/copyplash/model/Mappers.kt
+++ src/main/kotlin/com/artifactgames/copyplash/model/Mappers.kt
@@ -3,12 +3,16 @@ package com.artifactgames.copyplash.model
 import com.google.gson.Gson
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketMessage
+import java.io.File
+import java.io.FileReader
 
 
 val gson = Gson()
 
 
 fun <T> T.toJson(): String = gson.toJson(this)
+inline fun <reified T> String.fromJson(): T = gson.fromJson(this, T::class.java)
+inline fun <reified T> File.fromJsonFile(): T = gson.fromJson(FileReader(this), T::class.java)
 
 fun WebSocketMessage<*>.mapMessageToCommandRequest(): CommandRequest? =
     try {
--- src/main/kotlin/com/artifactgames/copyplash/model/RequestModels.kt
+++ src/main/kotlin/com/artifactgames/copyplash/model/RequestModels.kt
@@ -1,9 +1,10 @@
 package com.artifactgames.copyplash.model
 
 import com.artifactgames.copyplash.type.GameModes
-import com.artifactgames.copyplash.type.States
 
 
-data class GameMode(val gameMode: GameModes, val state: States)
+data class GameMode(val gameMode: GameModes, val numberOfRounds: Int = 0, val rounds: List<Round>? = null)
 
-data class GameSettings(val locale:String)
+data class Round(val position: Int, val title: String, val description: String, val timeout: Int)
+
+data class GameSettings(val locale:String, val gameMode: GameModes = GameModes.INSPIRATION)
